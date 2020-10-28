8721a341a975e8af5ef35ecf897a6f1c8f52c564
rongpenl <rongpenglee@gmail.com>
2020-10-28 15:49:31 -0700

    separate data and code; add necessary font

---
+++ frontend/js/data/constant.js
@@ -0,0 +1,61 @@
+const lax3Students = [
+  {
+    name: "Aydin Hasanli",
+    github_id: "aydin-hasanli",
+    capstone1_repo: "Tracking-the-Sun",
+  },
+  {
+    name: "Evgeny Grobov",
+    github_id: "evgenygrobov",
+    capstone1_repo: "AIRBNB_NYC",
+  },
+  {
+    name: "Joshua Chow",
+    github_id: "thejoshchow",
+    capstone1_repo: "coffee-arabica",
+  },
+  {
+    name: "Kacie Webster",
+    github_id: "kaciewebster",
+    capstone1_repo: "ca-fire-incidents",
+  },
+  {
+    name: "Mary MacCarthy",
+    github_id: "marymac17",
+    capstone1_repo: "capstone_one-",
+  },
+  {
+    name: "Opa Towobola",
+    github_id: "CarveTheFuture",
+    capstone1_repo: "TrendsInEnergyDevelopmentAndHealth",
+  },
+  {
+    name: "Zi Yang",
+    github_id: "zyang0009",
+    capstone1_repo: "DSIcap1HorsinAround",
+  },
+];
+
+const repoEndpoint = "https://api.github.com/repos/";
+
+const userEndpoint = "https://api.github.com/users/";
+
+const projectColNames = [
+  "Creator",
+  "GitHub User",
+  "Latest Update",
+  "Latest Commit",
+  "Total Commits",
+  "Programming Language",
+  "Number of Opened Issues",
+];
+
+const profileColNames = [
+  "Data Scientist Name",
+  "Location",
+  "Bio",
+  "Twitter UserName",
+  "Public Repos",
+  "Total Followers",
+  "Latest Active Time",
+];
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,64 +1,10 @@
-//global variables prefetched
-var lax3Students = [
-  {
-    name: "Aydin Hasanli",
-    github_id: "aydin-hasanli",
-    capstone1_repo: "Tracking-the-Sun",
-  },
-  {
-    name: "Evgeny Grobov",
-    github_id: "evgenygrobov",
-    capstone1_repo: "AIRBNB_NYC",
-  },
-  {
-    name: "Joshua Chow",
-    github_id: "thejoshchow",
-    capstone1_repo: "coffee-arabica",
-  },
-  {
-    name: "Kacie Webster",
-    github_id: "kaciewebster",
-    capstone1_repo: "ca-fire-incidents",
-  },
-  {
-    name: "Mary MacCarthy",
-    github_id: "marymac17",
-    capstone1_repo: "capstone_one-",
-  },
-  {
-    name: "Opa Towobola",
-    github_id: "CarveTheFuture",
-    capstone1_repo: "TrendsInEnergyDevelopmentAndHealth",
-  },
-  {
-    name: "Zi Yang",
-    github_id: "zyang0009",
-    capstone1_repo: "DSIcap1HorsinAround",
-  },
-];
-
-var repoEndpoint = "https://api.github.com/repos/";
-var userEndpoint = "https://api.github.com/users/";
-
-projectColNames = [
-  "Creator",
-  "GitHub User",
-  "Latest Update",
-  "Latest Commit",
-  "Total Commits",
-  "Programming Language",
-  "Number of Opened Issues",
-];
-
-profileColNames = [
-  "Data Scientist Name",
-  "Location",
-  "Bio",
-  "Twitter UserName",
-  "Public Repos",
-  "Total Followers",
-  "Latest Active Time",
-];
+// import {
+//   lax3Students,
+//   repoEndpoint,
+//   userEndpoint,
+//   projectColNames,
+//   profileColNames,
+// } from "./data/constant";
 
 var cap1RepoInfo = [];
 var cap1RepoCommits = [];
